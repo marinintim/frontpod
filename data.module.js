bbf01584f0f171d4c6ab5db19aadc94c0c6cd287
Tim Marinin <mt@marinin.xyz>
2017-07-24 00:38:48 +0300

    добавит data.module.js

---
+++ data.module.js
@@ -0,0 +1,27 @@
+const Data = {
+  podcasts: [
+    {
+      name: 'Фронтёрки',
+      slug: 'fronterki',
+      day: 'среда',
+      hosts: ['Тим Маринин'],
+      description: `Разговоры с фронтендерами.`,
+      icon: 'https://i1.sndcdn.com/artworks-000216694909-fb6o9d-t50x50.jpg',
+      url: 'https://fronterki.fireside.fm'
+    },
+    {
+      name: 'Веб-стандарты',
+      slug: 'webstandards',
+      day: 'понедельник',
+      hosts: [
+        'Вадим Макеев',
+        'Алексей Симоненко',
+        'Ольга Алексашенко'
+      ],
+      icon: 'https://i1.sndcdn.com/avatars-000202051699-d6qlyg-t50x50.jpg',
+      url: 'https://soundcloud.com/web-standards/'
+    }
+  ]
+}
+
+module.exports = Data
